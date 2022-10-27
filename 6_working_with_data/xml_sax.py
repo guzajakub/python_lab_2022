1ca44c2147be69efd63d8eaae9ae2a7042d4227d
guzajakub <guzajakub@student.agh.edu.pl>
2022-10-27 19:49:25 +0200

    xml sax exrrcise, change names for 5th lab

---
+++ 6_working_with_data/xml_sax.py
@@ -0,0 +1,52 @@
+import xml.sax
+
+
+class XMLHandler(xml.sax.ContentHandler):
+
+    def __init__(self):
+        self.current = ""
+        self.name = ""
+        self.price = ""
+        self.description = ""
+        self.calories = ""
+
+    def startElement(self, name, attrs):
+        self.current = name
+        if name == "food":
+            print(f"{attrs['id']} food is: ")
+
+    def characters(self, content):
+        if self.current == "name":
+            self.name = content
+        elif self.current == "price":
+            self.price = content
+        elif self.current == "description":
+            self.description = content
+        elif self.current == "calories":
+            self.calories = content
+
+    def endElement(self, attrs):
+        if self.current == "name":
+            print(f"Name: {self.name}")
+        elif self.current == "price":
+            print(f"Price: {self.price}")
+        elif self.current == "description":
+            print(f"Description: {self.description}")
+        elif self.current == "calories":
+            print(f"Calories: {self.calories}")
+        self.current = ""
+
+    # def changeElement(self, name, attrs):
+    #     self.current = name
+    #     if name == "food":
+    #         print(f"{attrs['id']} food is: ")
+
+
+if __name__ == "__main__":
+    # basic parser for xml sax
+    parser = xml.sax.make_parser()
+    # make object of our own xml handler
+    xml_handler = XMLHandler()
+    # set content handler for xml.sax parser, giving our XMLHandler class
+    parser.setContentHandler(xml_handler)
+    parser.parse("sample_xml.xml")
