572167513e059b25452ecde7e3a6b429cbe0b8c7
guzajakub <guzajakub@student.agh.edu.pl>
2022-11-06 14:38:34 +0100

    histogram done v2

--- 7_parallel_calculations/histogram.py
+++ 7_parallel_calculations/histogram.py
@@ -8,12 +8,12 @@ def square(a):
     return a*a
 
 
-def calculate_pythagoras_histogram(num_runs=50):
+def calculate_pythagoras_histogram(num_runs):
     pythagoras = np.zeros(num_runs, dtype=float)
     for idx in range(num_runs):
         pythagoras[idx] = square(idx) + square(idx)
-    x_axis = list(pythagoras)
-    y_axis = list(range(0, num_runs))
+    y_axis = list(pythagoras)
+    x_axis = list(range(0, num_runs))
 
     return x_axis, y_axis
 
@@ -21,20 +21,20 @@ def calculate_pythagoras_histogram(num_runs=50):
 def main():
     start = time.time()
     with concurrent.futures.ThreadPoolExecutor(max_workers=2) as thread:
-        th = thread.submit(calculate_pythagoras_histogram, 50)
+        th = thread.submit(calculate_pythagoras_histogram, 1_000_000)
     x_axis, y_axis = th.result()
     end = time.time()
-    print(f"without threading: {end-start}secs")
+    print(f"with threading: {end-start}secs")
 
     start = time.time()
-    x_axis, y_axis = calculate_pythagoras_histogram(50)
+    x_axis, y_axis = calculate_pythagoras_histogram(1_000_000)
     end = time.time()
     print(f"without threading: {end-start}secs")
 
     # print(x_axis, y_axis)
-    # fig, ax = plt.subplots()
-    # ax.stem(x_axis, y_axis)
-    # plt.show()
+    fig, ax = plt.subplots()
+    ax.stem(x_axis, y_axis)
+    plt.show()
 
 
 if __name__ == '__main__':
